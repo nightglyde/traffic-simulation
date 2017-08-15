09ffd97a23aa79bde3b8f8fbebd88700e93041fc
nightglyde <21495335@student.uwa.edu.au>
2017-08-15 12:41:00 +0800

    the angles work without jiggling

--- car.py
+++ car.py
@@ -16,7 +16,6 @@ BLUE    = (  0,   0, 255)
 MAGENTA = (255,   0, 255)
 
 CAR_SIZE   = 10
-MULTIPLIER = 100000
 
 plan = [[600, 400], [300, 600], [1100, 200], [200, 100], [800, 500]]
 
@@ -29,7 +28,7 @@ class Vector2:
         return Vector2(-self.y, self.x)
 
     def coords(self):
-        return [self.x//MULTIPLIER, self.y//MULTIPLIER]
+        return [self.x, self.y]
 
     def __add__(self, other):
         return Vector2(self.x + other.x, self.y + other.y)
@@ -49,6 +48,25 @@ class Vector2:
     def __repr__(self):
         return repr([self.x, self.y])
 
+def getVector(angle):
+    return Vector2(math.cos(angle), math.sin(angle))
+
+def getAngle(vector):
+    x = vector.x
+    y = vector.y
+
+    if x == 0:
+        angle = math.pi / 2
+    else:
+        angle = math.atan(y / x)
+
+    if x < 0:
+        angle += math.pi
+    elif y < 0:
+        angle += math.pi * 2
+
+    return angle
+
 class Car:
     def __init__(self, screen, colour, position,
                  speed=100, car_angle=0):
@@ -56,22 +74,17 @@ class Car:
         self.screen = screen
         self.colour = colour
 
-        self.position = Vector2(*position)*MULTIPLIER
-        self.heading = Vector2(*util.side_lengths[car_angle])
+        self.position = Vector2(*position)
+        self.heading  = getVector(car_angle)
 
         self.speed     = speed
         self.car_angle = car_angle
 
         self.instructions = []
         for item in plan:
-            #corners = []
-            #corners.append((item + Vector2( 10,  10))*MULTIPLIER)
-            #corners.append((item + Vector2( 10, -10))*MULTIPLIER)
-            #corners.append((item + Vector2(-10,  10))*MULTIPLIER)
-            #corners.append((item + Vector2(-10, -10))*MULTIPLIER)
-            #self.instructions.append(item, corners)
-            self.instructions.append(Vector2(*item) * MULTIPLIER)
+            self.instructions.append(Vector2(*item))
         self.instructions.reverse()
+        self.instructions.pop()
 
         self.path = [self.position.coords()]
         self.time = pygame.time.get_ticks()
@@ -83,68 +96,30 @@ class Car:
         print("dt:", dt)
         self.time = time
 
-        self.position += self.heading * self.speed * dt // 1000
+        self.position += self.heading * self.speed * dt / 1000
         self.path.append(self.position.coords())
 
         # follow instructions
         while self.instructions:
-            #corners = self.instructions[-1]
             target_position = self.instructions[-1]
 
-            if abs(self.position.x-target_position.x) < MULTIPLIER*10:
-                if abs(self.position.y-target_position.y) < MULTIPLIER*10:
-                    print("DONE", target_position)
-                    self.instructions.pop()
-                    continue
-
             to_dest = target_position - self.position
-            turn = self.heading.x * to_dest.y - self.heading.y * to_dest.x
 
-            if turn < 0:
-                self.car_angle -= (dt / 1000) * 45
-            elif turn > 0:
-                self.car_angle += (dt / 1000) * 45
+            if abs(to_dest.x) < 10 and abs(to_dest.y) < 10:
+                print("DONE", self.position, target_position)
+                self.instructions.pop()
+                continue
 
+            angle_difference = (self.car_angle - getAngle(to_dest)) % (math.pi * 2)
+
+            if 0 < angle_difference < math.pi:
+                self.car_angle -= min(dt / 1000, angle_difference)
+            elif angle_difference >= math.pi:
+                self.car_angle += min(dt / 1000, (math.pi*2) - angle_difference)
+
+            self.heading = getVector(self.car_angle)
             break
 
-            #change = False
-
-            #if target_speed != None:
-            #    if target_speed > self.speed:
-            #        self.speed += 1
-            #        change = True
-            #    elif target_speed < self.speed:
-            #        self.speed -= 1
-            #        change = True
-
-            #if target_angle != None:
-            #    angle_difference = (self.car_angle-target_angle)%360
-            #    if 0 < angle_difference < 180:
-            #        self.car_angle -= 1
-            #        change = True
-            #    elif angle_difference >= 180:
-            #        self.car_angle += 1
-            #        change = True
-
-            #if target_time != None:
-            #    if target_time > self.timer:
-            #        self.timer += 1
-            #        change = True
-
-            #if change:
-            #    self.instructions.append((target_speed, target_angle, target_time))
-            #    break
-
-            #else:
-            #    print("DONE", target_speed, target_angle, target_time)
-            #    self.timer = 0
-
-        # update direction
-        self.car_angle %= 360
-        #self.heading = Vector2(*util.side_lengths[round(self.car_angle)])
-        angle = math.radians(self.car_angle)
-        self.heading = Vector2(MULTIPLIER * math.cos(angle),
-                               MULTIPLIER * math.sin(angle))
 
     def draw(self):
         forward = self.heading * 5 * CAR_SIZE
