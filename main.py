b9fdf3c73311878a38e2ad0c829dd92247f824b4
nightglyde <21495335@student.uwa.edu.au>
2019-06-14 20:19:18 +0800

    Added comment with controls

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import src.run_demo_simulation
 
 src.run_demo_simulation.main()
 
+# Controls:
+# click and drag - move camera
+# scroll wheel   - camera zoom
+# click on car   - follow car
+# spacebar       - pause/resume
+# escape         - reset camera position and zoom
